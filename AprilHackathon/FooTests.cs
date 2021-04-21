c1127bc6dfe0b1639a49897eaa64003e1ae6a4e3
petr.sapak <petr.sapak@thermofisher.com>
2021-04-21 14:58:04 +0200

    Add functionality and one test

---
+++ AprilHackathon/FooTests.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+
+namespace AprilHackathon
+{
+    [TestFixture]
+    class FooTests
+    {
+        [Test]
+        public void ReturnHelloWorldTest()
+        {
+            Assert.That(new Foo().ReturnHelloWorld(), Is.EqualTo("Hello World"));
+        }
+    }
+}
