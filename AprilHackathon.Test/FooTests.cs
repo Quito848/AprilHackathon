d02f8331391c49ac77eee61c28ab046c0140c443
petr.sapak <petr.sapak@thermofisher.com>
2021-04-21 15:44:53 +0200

    Add test for the second method

--- AprilHackathon.Test/FooTests.cs
+++ AprilHackathon.Test/FooTests.cs
@@ -1,9 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using NUnit.Framework;
+﻿using NUnit.Framework;
 
 namespace AprilHackathon.Test
 {
@@ -14,6 +9,12 @@ namespace AprilHackathon.Test
         public void ReturnHelloWorldTest()
         {
             Assert.That(new Foo().ReturnHelloWorld(), Is.EqualTo("Hello World"));
+        }
+
+        [Test]
+        public void ReturnTwoTest()
+        {
+            Assert.That(new Foo().ReturnTwo(), Is.EqualTo(2));
         }
     }
 }
